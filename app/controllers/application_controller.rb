fd52f4fa3d033e10d50a72f8d016dc9931398048
keiadachi <2013edcckeiadachi@gmail.com>
2021-03-14 17:25:13 +0000

    3/14bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+      if current_user
+        flash[:notice] = "ログインに成功しました" 
+        root_url  #　指定したいパスに変更
+      else
+        flash[:notice] = "新規登録完了しました。次に名前を入力してください" 
+        new_profile_path  #　指定したいパスに変更
+      end
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+  
 end
